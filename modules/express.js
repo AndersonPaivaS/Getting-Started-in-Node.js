9fb675b4df653e7bd3df5897bf9c6cdf7d130b36
Your Name <you@example.com>
2023-08-25 13:01:51 -0300

    feat: create user capture endpoint

--- modules/express.js
+++ modules/express.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
 const UserModel = require('../src/models/user.model')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 const port = 8080
 
@@ -12,22 +12,28 @@ app.get("/home", (req, res) => {
     res.status(200).send("<h1>Inicializing server with Express</h1>")
 })
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name: 'João Maria Ribeiro',
-            age: 47
-        }, 
-        {
-            name: 'Jorge Vicente da Silva',
-            age: 21
-    }]
-    res.status(200).json(users)
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({})
+        res.status(200).json(users)
+    } catch(error){
+        res.status(500).send(error.message)
+    }
+})
+
+app.get("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await UserModel.findById(id)
+        res.status(200).json(user)
+    } catch(error){
+        res.status(500).send(error.message)
+    }
 })
 
 app.post("/users", async (req, res) => {
     try {
-        const user = await UserModel.create(req.body);
+        const user = await UserModel.create(req.body)
         res.status(201).json(user)
     } catch(error) {
         res.status(500).send(error.message)
