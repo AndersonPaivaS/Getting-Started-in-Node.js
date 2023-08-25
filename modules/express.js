fc565039a7d25c974c98c34bc3fbfca54729aa88
Your Name <you@example.com>
2023-08-25 13:44:45 -0300

    feat: create user update endpoint

--- modules/express.js
+++ modules/express.js
@@ -31,6 +31,16 @@ app.get("/users/:id", async (req, res) => {
     }
 })
 
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(200).json(user)
+    } catch(error){
+        res.status(500).send(error.message)
+    }
+})
+
 app.post("/users", async (req, res) => {
     try {
         const user = await UserModel.create(req.body)
